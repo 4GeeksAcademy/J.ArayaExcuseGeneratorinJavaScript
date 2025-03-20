048e2133878ed7b0af0dc20174abcd5671c7525f
ImJAraya <fabiarayamontero@gmail.com>
2025-03-20 16:42:49 -0600

    Proyecto Terminado

---
+++ index.js
@@ -0,0 +1,18 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let excuses = [who, action, what, when];
+
+
+
+function randomExcuse() {
+    let randomIndex = '';
+
+    for (let i = 0; i < excuses.length; i++) {
+        randomIndex += excuses[i][Math.floor(Math.random() * excuses[i].length)]+' ';
+        console.log(randomIndex);
+    };
+    document.getElementById("excusa").textContent = randomIndex;
+};
+document.addEventListener("DOMContentLoaded", randomExcuse);
