0c39924a83bbb57e9787242bdaaf6fc7a0bf0b68
ImJAraya <fabiarayamontero@gmail.com>
2025-03-20 18:05:02 -0600

    Corregido, todo trabajado desde src

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let excuses = [who, action, what, when];
+  let randomIndex = '';
 
+  for (let i = 0; i < excuses.length; i++) {
+    randomIndex += excuses[i][Math.floor(Math.random() * excuses[i].length)] + ' ';
+    console.log(randomIndex);
+  };
+  document.getElementById("excusa").textContent = randomIndex;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
 };
